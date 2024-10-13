4518fd0875f59166c31a8837b01dbafc328dd689
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-10-13 12:28:09 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -36,7 +36,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  domain_name_label   = "aaidavm678"
+  domain_name_label   = "aaidavm6788"
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {

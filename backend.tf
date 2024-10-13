12efbd5140f4ad07c7f09cebb165e87734eda884
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-10-13 12:04:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -13,6 +13,5 @@ terraform {
     storage_account_name = "terraformstateaaida23"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
   }
 }

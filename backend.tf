37d96cba260d5783632e021b32486d7b434f9665
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-10-13 11:49:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,4 +2,11 @@ terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
+backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformstateaaida23"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true 
+}
 }

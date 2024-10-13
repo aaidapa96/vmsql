073db81bcdb5a5a9766b7154baf65d7a21dfd00a
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-10-13 11:21:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,3 +7,19 @@ provider "azurerm" {
 resource "random_id" "random" {
   byte_length = 4
 }
+
+data "terraform_remote_state" "foo" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformstateaaida23"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    client_id            = secret.AZURE_CLIENT_ID  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    client_secret        =   # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    subscription_id      = secrets.AZURE_SUBSCRIPTION_ID  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    tenant_id            = secrets.AZURE_TENANT_ID  # Can also be set via `ARM_TENANT_ID` environment variable.
+    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+  }
+}
+
